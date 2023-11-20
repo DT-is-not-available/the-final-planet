72a8188f3a68c125e93ac985f4bd641f99226592
DT <66632141+DT-is-not-available@users.noreply.github.com>
2023-11-20 12:13:44 -0500

    added mod credits to the credits menu

---
+++ credits.js
@@ -0,0 +1,32 @@
+var old = gui_CreditsWindow.create
+gui_CreditsWindow.create = function(city,gui,stage,$window) {
+    const windowAddBottomButtons = gui.windowAddBottomButtons
+    gui.windowAddBottomButtons = function(){}
+    old.call(this, city,gui,stage,$window)
+    gui.windowAddBottomButtons = windowAddBottomButtons
+    const credits = `
+    
+    #The Final Planet Mod
+    
+    Created by DT
+    Other Lines of Credit Go Here
+    Things like "this guy did sprites"
+
+    Other Contributors
+    this is where we list the people who did minor pull requests and such
+    `.split("\n")
+    for (let i = 0; i < credits.length-1; i++) {
+        if (credits[i].trimStart() == "") {
+            $window.addChild(new gui_GUISpacing($window,new common_Point(2,6)));
+        } else {
+            let line = credits[i].trimStart()
+            let font = "Arial"
+            if (line.startsWith("#")) {
+                line = line.replace("#","")
+                font = "Arial16"
+            }
+            $window.addChild(new gui_TextElement($window,stage,line,null,font));
+        }
+    }
+    gui.windowAddBottomButtons()
+}
