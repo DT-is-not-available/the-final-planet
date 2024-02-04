c176dd9f8775ef34cdb6c6abf5bc861f18a13a9f
DT <66632141+DT-is-not-available@users.noreply.github.com>
2024-02-04 00:31:10 -0500

    n

--- dontAutoLoad/main.js
+++ dontAutoLoad/main.js
@@ -1,3 +1,1 @@
-alert(tfe2.ModTools)
-
-// TODO: move old index.js and credits.js into here and refactor with extend
+console.log(Liquid.getMod())
--- modController.js
+++
@@ -1,5 +0,0 @@
-global.tfe2 = window
-ModTools.onLoadStart(function(city) {
-	global.tfe2.game = city.game
-})
-require(decodeURI(document.currentScript.src.replace("modController.js", "dontAutoLoad/main.js").replace("file:///", "")))
